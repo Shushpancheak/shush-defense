029f7eba0eb8d59950f742eab3e5bea3d3d36cd8
Maximelian Mumladze <Makcieml@gmail.com>
2021-02-09 23:00:56 +0300

    first hw

---
+++ Assets/Scripts/NewtonGravity.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class NewtonGravity : MonoBehaviour
+{
+    private const float MIN_DISTANCE = 0.1f;
+    
+    [SerializeField] private Vector3 m_velocity    = new Vector3(1, -1, 0.2f);
+    
+    [SerializeField] private Vector3 m_pos_target  = new Vector3(0, 0, 0);
+    [SerializeField] private float   m_mass_target = 700f;
+
+    [SerializeField] private float   G = 6.67f * 1e-2f;
+    
+    private float G_MASS_TARGET = 0;
+
+    void Start()
+    {
+        G_MASS_TARGET = G * m_mass_target;
+    }
+
+    void FixedUpdate()
+    {
+        // Compute acceleration:
+        // a = G * M * (R - r) / ((R - r)^2 * |R - r|)
+        
+        // R - r
+        var delta = m_pos_target - transform.position;
+        var delta_magnitude = delta.magnitude;
+
+        if (delta_magnitude < MIN_DISTANCE)
+        {
+            return;
+        }
+        
+        var acceleration = delta * (G_MASS_TARGET / Mathf.Pow(delta_magnitude, 3));
+    
+        transform.Translate(m_velocity * Time.fixedDeltaTime + acceleration * Mathf.Pow(Time.fixedDeltaTime, 2) / 2);
+        
+        m_velocity += acceleration * Time.fixedDeltaTime;
+    }
+}
